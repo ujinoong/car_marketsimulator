b0953daa87a04161021a99928be6dd4ef4bbd85a
Yu Jin HONG <hyjno254@gmail.com>
2026-09-17 22:23:31 +0900

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Virtual Auto Market",description:"AI Customer Market Simulator"};
+export default function RootLayout({children}){return <html lang="ko"><body>{children}</body></html>}
